cdfa35cf439684d8e36429b2c2b3278b9b44e51f
CrazyABCHacker <pritam.sh.2020@gmail.com>
2021-04-09 22:50:13 +0530

    Final Commit2

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -6,8 +6,11 @@ public class GameFrame extends JFrame{
     GamePanel panel;
 
     GameFrame(){
+        ImageIcon icon = new ImageIcon("assets/Ping_Pong.jpg");
+
         panel = new GamePanel();
         this.add(panel);
+        this.setIconImage(icon.getImage());
         this.setTitle("Pong Game");
         this.setResizable(false);
         this.setBackground(Color.black);
